c09a669f0bdd3a5dd534113af9eea2d5cdfe6329
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-10-15 22:15:15 -0300

    Step 8: Setup auth-screen when not logged in

--- navigation/index.js
+++ navigation/index.js
@@ -1,12 +1,39 @@
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import auth from '@react-native-firebase/auth';
 
 import Homepage from "../pages/homepage";
+import Authpage from "../pages/auth";
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
+    const [initializing, setInitializing] = React.useState(true);
+    const [user, setUser] = React.useState();
+
+    // change user to logged in
+    const onAuthStateChanged = (userData) => {
+        setUser(userData)
+        if (initializing) setInitializing(false)
+    }
+
+    React.useEffect(() => {
+        return auth().onAuthStateChanged(onAuthStateChanged);
+    }, [])
+
+    if (initializing) return null;
+
+    if (!user) {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Auth" component={Authpage} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        )
+    }
+
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
---
+++ pages/auth/style.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const Styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 32,
+        textAlign: 'center',
+    }
+})
+
+export default Styles;
