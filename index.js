2f3fa2900f5869b469eb9f2ff6078319f094aa3e
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-11-10 07:44:23 -0300

    enable gesture-handler

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+// enable gesture-handler in app.
+import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => App);
