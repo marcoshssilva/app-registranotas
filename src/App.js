6c1ff73154609ff5adde6988091a5b15876043ae
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-11-13 10:10:23 -0300

    Implements initial prefs save in AsyncLocalStorage.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import RootNavigator from "./navigation";
 import { Provider as PaperProvider } from "react-native-paper";
 import theme from './theme';
-import { StatusBar } from 'react-native';
+import { StatusBar, BackHandler } from 'react-native';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import {ModalCreateFolder} from "./components/modal-create-folder";
 import {ModalSettingsConfig} from "./components/modal-settings-config";
 import {ModalCreateNotes} from "./components/modal-create-notes";
+import * as LocalStorage from "./services/local-storage";
 
 GoogleSignin.configure({
     webClientId: '%REPLACE_ID_HERE%', // gooogle-services.json -> oauth_client.client_id
     offlineAccess: true
 });
+
+LocalStorage.initializeConfig().catch((err) => {
+    console.log(err);
+    BackHandler.exitApp()
+})
 
 const App = () => (
     <PaperProvider theme={theme}>
---
+++ src/services/local-storage/index.js
@@ -0,0 +1,17 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const DEFAULT_PREFS_INDEX = 'intern'
+
+const INITIAL_DEFAULT_PREFs =  {}
+
+export const initializeConfig = async () => {
+    return await AsyncStorage.setItem(DEFAULT_PREFS_INDEX, INITIAL_DEFAULT_PREFs)
+}
+
+export const getKeyValue = async (key) => {
+    return await AsyncStorage.getItem(key)
+}
+
+export const setKeyValue = async (key, newValue) => {
+    return await AsyncStorage.setItem(key, newValue)
+}
