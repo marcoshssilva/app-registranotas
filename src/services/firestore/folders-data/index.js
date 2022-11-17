a68738679a7db980085d00c7db5699eaae604ef1
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-11-17 10:31:23 -0300

    Fix: error modal when user not logged in.

--- src/services/firestore/folders-data/index.js
+++ src/services/firestore/folders-data/index.js
@@ -3,8 +3,6 @@ import firestore from "@react-native-firebase/firestore";
 import {DOCUMENTDATA_COLLECTION} from "../document-data";
 import {randomUUID} from "../../utils/uid";
 
-const USERNAME_LOGGED = auth().currentUser.email;
-
 export const FOLDER_TYPE_PUBLIC = 'public';
 export const FOLDER_TYPE_SHARED = 'shared';
 export const FOLDER_TYPE_PRIVATE = 'private';
@@ -13,11 +11,13 @@ export const FOLDER_TYPE_PRIVATE = 'private';
  * @returns {Promise<[]>}
  */
 export const getAllFolders = async () => {
+    const USERNAME_LOGGED = auth().currentUser.email;
     return (await firestore().collection(DOCUMENTDATA_COLLECTION).doc(USERNAME_LOGGED).get())
         .data()?.data.folders
 }
 
 export const addFolder = async (folderName, folderDescription, folderVisibility) => {
+    const USERNAME_LOGGED = auth().currentUser.email;
     // validation folder type
     if (![FOLDER_TYPE_PUBLIC, FOLDER_TYPE_SHARED, FOLDER_TYPE_PRIVATE].includes(folderVisibility)) throw new Error("Invalid folderVisibility");
 
